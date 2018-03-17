e3f86fd9224c753bf34a04e96b64cde100588e69
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-03-17 16:13:07 +0700

    Remove Semantic UI and add Gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const srcmaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const minifyjs = require('gulp-js-minify');
+
+gulp.task('sass', function() {
+	return gulp.src('src-styles/main.sass')
+		.pipe(srcmaps.init())
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(rename('style.css'))
+		.pipe(srcmaps.write('./'))
+		.pipe(gulp.dest('./public/styles/'));
+})
+
+gulp.task('js', function(){
+	return gulp.src('src-scripts/main.js')
+	.pipe(srcmaps.init())
+	.pipe(minifyjs())
+	.pipe(rename('main.js'))
+	.pipe(srcmaps.write('./'))
+	.pipe(gulp.dest('./public/scripts/'));
+ });
+
+gulp.task('watch', function() {
+	gulp.watch('src-styles/**/*.sass', ['sass']);
+	gulp.watch('src-scripts/**/*.js', ['js']);
+	console.log('Watching for changes...');
+})
+
+gulp.task('default', ['watch']);
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Set view location
+app.set('views', './views');
+// Set template engine
+app.set('view engine', 'pug');
+// Set publicly-accessible path
+app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(3000, () => console.log('Listening at port 3000\nPress Ctrl+C to stop'));
+// Routes
+app.get('/', function(req, res) {
+	res.render('index', {title: 'Hello'});
+});
+
+// Start the server
+app.listen(3000, () => console.log('Listening on port 3000\nPress Ctrl+C to stop'));
