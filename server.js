0e043a9463a4dbb7713c8c95711a65f8c69384c4
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-03-30 10:37:27 +0700

    Add manage-role-single page

--- server.js
+++ server.js
@@ -103,6 +103,12 @@ app.get("/manage-role", function(req, res) {
 		user: user
 	});
 });
+app.get("/manage-role/:id", function(req, res) {
+		res.render("manage-role-single", {
+		session: session,
+		user: user
+	});
+});
 app.get("/manage-role/:id/users", function(req, res) {
 		res.render("manage-role-user", {
 		session: session,
