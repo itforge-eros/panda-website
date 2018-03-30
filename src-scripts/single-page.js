1bd7d1960b8281238fb0692e3d6bef5d438bdb23
Foofy <foozaasksksk@gmail.com>
2018-03-30 23:46:07 +0700

    convert time to humanity format in request-single page

--- src-scripts/single-page.js
+++ src-scripts/single-page.js
@@ -1,3 +1,21 @@
+function converterToHour(endTime, startTime){
+	return Math.floor(endTime - startTime); 
+	// this function use for finding duration of room reservation with hour that be floored
+}
+function converterOverToMins(endTime, startTime){
+	return (((endTime - startTime) - Math.floor(endTime - startTime)) * 60);
+	// this function use for finding duration of room servation by bring the overtime from hour convert to minutes
+}
+function convertToHumanity(time) {
+	if (time%1 !== 0){
+		return Math.floor(time) + ':' +((time) - Math.floor(time))*60;
+	}else {
+		return Math.floor(time);
+	}
+	
+	// this function use for converting the format .5 hour to humanity format
+}
+
 Vue.component('time-slot', {
 	props: {
 		available: Boolean,
@@ -18,7 +36,6 @@ Vue.component('time-slot', {
 		}
 	}
 });
-
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -29,7 +46,8 @@ var app = new Vue({
 		chosenTimePeriod: function () {
 			let startTime = Math.min.apply(null, this.chosenTimes);
 			let endTime = Math.max.apply(null, this.chosenTimes);
-			return startTime + '-' + endTime + ' น.'
+			return convertToHumanity(startTime) + '-' + convertToHumanity(endTime) + ' น. (' + converterToHour(endTime, startTime) +' ชั่วโมง '+ converterOverToMins(endTime, startTime) + ' นาที)'
 		}
 	}
 });
+
