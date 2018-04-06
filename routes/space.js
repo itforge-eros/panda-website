967d09999af8df9621140adc9d955668ec91cc1d
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-06 17:36:06 +0700

    Get single space info from API

--- routes/space.js
+++ routes/space.js
@@ -1,13 +1,42 @@
+const globalVars = require("../globalVars");
 const express = require("express");
 const router = express.Router();
 const testData = require("../models/testData");
 
-router.get("/:id", (req, res) => {
-	res.render("single-space", {
-		session: testData.session,
-		user: testData.user,
-		faculty: testData.faculty
+const ApolloClient = require("apollo-client").ApolloClient;
+const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
+const createHttpLink = require("apollo-link-http").createHttpLink;
+const fetch = require("node-fetch");
+const gql = require("graphql-tag");
+
+const apollo = new ApolloClient({
+	uri: globalVars.apiURL,
+	link: createHttpLink({ uri: globalVars.apiURL, fetch: fetch }),
+	cache: new InMemoryCache()
+});
+
+const getSpace = id => {
+	return apollo.query({
+		query: gql`
+			{
+				space(id: "${id}") {
+					id, name, description, capacity, isAvailable
+				}
+			}
+		`
 	});
+};
+
+router.get("/:id", (req, res) => {
+	getSpace(req.params.id)
+		.then(returnedData => {
+			res.render("single-space", {
+				session: testData.session,
+				user: testData.user,
+				faculty: testData.faculty,
+				space: returnedData.data.space
+			});
+		});
 });
 
 module.exports = router;
