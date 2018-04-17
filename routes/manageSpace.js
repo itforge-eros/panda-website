756130178f94c68d2ddf91822403577940242edb
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-17 21:03:43 +0700

    Add an array of amenities

--- routes/manageSpace.js
+++ routes/manageSpace.js
@@ -2,18 +2,26 @@ const express = require("express");
 const router = express.Router();
 const testData = require("../models/testData");
 
+const amenities = [
+	{ id: "proj", name: "โปรเจ็กเตอร์" },
+	{ id: "aircon", name: "แอร์" },
+	{ id: "sound", name: "ระบบเสียง" },
+	{ id: "pc", name: "คอมฯ ผู้สอน" }
+];
+
 router.get("/", (req, res) => {
-		res.render("manage-space", {
+	res.render("manage-space", {
 		session: testData.session,
 		user: testData.user,
 		member: req.session.member
 	});
 });
 router.get("/:id", (req, res) => {
-		res.render("manage-space-single", {
+	res.render("manage-space-single", {
 		session: testData.session,
 		user: testData.user,
-		member: req.session.member
+		member: req.session.member,
+		amenities: amenities
 	});
 });
 
