8addfd339d0c787e728d6b85d09f9d65c1e6ead0
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-19 14:56:36 +0700

    Add department selection for administrative accounts

--- routes/authen.js
+++ routes/authen.js
@@ -10,11 +10,28 @@ const testData = require("../models/testData");
 const ApolloClient = require("apollo-client").ApolloClient;
 const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
 const createHttpLink = require("apollo-link-http").createHttpLink;
+const setContext = require("apollo-link-context").setContext;
 const fetch = require("node-fetch");
 const gql = require("graphql-tag");
 
+// token for apollo
+let token = "";
+
+router.use((req, res, next) => {token = req.session.token; next()});
+
 const apollo = new ApolloClient({
 	link: createHttpLink({ uri: globalVars.gqlURL, fetch: fetch }),
+	cache: new InMemoryCache()
+});
+
+const authLink = setContext((_, { headers }) => {
+	return { headers: { authorization: token ? `bearer${token}` : "" } };
+});
+
+const apollo_auth = new ApolloClient({
+	link: authLink.concat(
+		createHttpLink({ uri: globalVars.gqlURL, fetch: fetch })
+	),
 	cache: new InMemoryCache()
 });
 
@@ -24,9 +41,28 @@ const sendLogin = (usr, pwd) => {
 			{
 				login(username: "${usr}", password: "${pwd}") {
 					member {
-						id, username, firstName, lastName, email, roles { departments }
+						id, username, firstName, lastName, email
 					},
 					token
+				}
+			}
+		`
+	});
+};
+
+const getMe = () => {
+	return apollo_auth.query({
+		query: gql`
+			{
+				me {
+					roles {
+						department {
+							name fullThaiName description
+						}
+						permissions {
+							accesses
+						}
+					}
 				}
 			}
 		`
@@ -61,18 +97,29 @@ router.get("/login", (req, res, next) => {
 router.post("/login", multer().array(), (req, res, next) => {
 	if (req.body.username == "" || req.body.password == "") {
 		isCredEmpty = true;
-		res.redirect("/authen/login");
+		res.redirect("/authen/login/");
 	} else {
-		sendLogin(req.body.username, req.body.password).then(data => {
-			req.session.token = data.data.login.token;
-			req.session.member = data.data.login.member;
-			res.redirect("/");
-		}).catch(err => {
-			// console.log(err.graphQLErrors[0].message);
-			if (globalVars.env != "production") console.log(err);
-			isCredInvalid = true;
-			res.redirect("/authen/login");
-		});
+		sendLogin(req.body.username, req.body.password)
+			.then(data => {
+				req.session.token = data.data.login.token;
+				req.session.member = data.data.login.member;
+				token = req.session.token;
+				getMe().then(meData => {
+					req.session.member = Object.assign({}, req.session.member, meData.data.me);
+					if (req.session.member.roles.length > 1)
+						res.redirect("/choose-dept/")
+					else
+						res.redirect("/");
+				}).catch(err => {
+					if (globalVars.env != "production") console.log(err);
+				})
+			})
+			.catch(err => {
+				// console.log(err.graphQLErrors[0].message);
+				if (globalVars.env != "production") console.log(err);
+				isCredInvalid = true;
+				res.redirect("/authen/login/");
+			});
 	}
 });
 
