341dea888cd4183dcd1148a163b3e0f6e2a9f4f4
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-23 14:53:41 +0700

    Use native ES6 array prototype for authen helper

--- helpers/authen.js
+++ helpers/authen.js
@@ -1,10 +1,4 @@
-const hasAllAccess = (my, required) => {
-	let flag = false;
-	for(var i = 0; i < required.length; i++)
-		if (!my.includes(required[i])) {flag = true; break}
-	return !flag
-};
-
+const hasAllAccess = (my, required) => required.every(r => my.includes(r));
 const hasEitherAccess = (my, required) => required.some(r => my.includes(r));
 
 module.exports = {
