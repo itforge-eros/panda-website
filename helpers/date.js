af38ac39c0a2609eac68618a9d4f0455da38a81f
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-23 13:24:15 +0700

    Add slotToTime Date helper

--- helpers/date.js
+++ helpers/date.js
@@ -1,4 +1,7 @@
 const monthsTh = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."];
+const times = [];
+for (let t = 0, i = 0; t <= 23.5; t+=0.5, i++) times.push({slot: i, time: t});
+
 const epochToDate = epoch => new Date(epoch * 1000);
 const bigEndianToDate = x => {
 	x = x.split("-");
@@ -6,6 +9,10 @@ const bigEndianToDate = x => {
 }
 const thaiDateOf = x => {
 	return x.getDate() + " " + monthsTh[x.getMonth()] + " " + x.getFullYear()
+}
+const slotToTime = slot => {
+	if (slot % 2 == 0) return times[slot].time.toString() + ":00";
+	else return times[slot].time.toString().replace(".5", ":30");
 }
 
 module.exports = {
