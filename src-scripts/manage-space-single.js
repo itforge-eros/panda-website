35717f80601c546fd174df6defd0119c240120d8
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-04-27 14:31:14 +0700

    Add dropzone structure

--- src-scripts/manage-space-single.js
+++ src-scripts/manage-space-single.js
@@ -2,11 +2,30 @@ var app = new Vue({
 	el: "#app",
 	data: {
 		spaceName: "",
-		spaceURI: ""
+		spaceURI: "",
+		uploadBoxIsOpen: false
 	},
 	computed: {
 		computedSpaceURI: function() {
 			return this.spaceURI.toLowerCase().replace(/ /g, "-")
 		}
+	},
+	methods: {
+		toggleUploadBox: function() {
+			this.uploadBoxIsOpen = !this.uploadBoxIsOpen;
+			// if isOpen => call for upload URL and create dropzone
+			// is isClosed => clear queue and remove dropzone
+		}
 	}
 });
+
+function createDropzone(url) {
+	var imgDropzone = new Dropzone("div#imgDropzone", {
+		url: url,
+		maxFiles: 1,
+		acceptedFiles: "image/*",
+		dictDefaultMessage: "ลากภาพมาวางที่นี่ (อัพโหลดได้ 1 ภาพ)",
+		dictMaxFilesExceeded:"เกินจำนวนไฟล์ที่อนุญาต",
+		dictInvalidFileType: "ไม่ใช่รูปภาพ"
+	});
+}
