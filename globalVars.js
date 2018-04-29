fbda38c9d5837deae6eef7f98e9fc77783025d03
kavinvin <kavinvin.vin@gmail.com>
2018-04-29 21:28:26 +0700

    Get API host from environment variable

--- globalVars.js
+++ globalVars.js
@@ -1,10 +1,7 @@
 const session = require("express-session");
 const MemoryStore = require("memorystore")(session);
 
-const apiHostname =
-	process.env.NODE_ENV == "production"
-		? "http://127.0.0.1:9000"
-		: "https://api.space.itforge.io";
+const apiHostname = process.env.PANDA_API_HOST || "https://api.space.itforge.io";
 const gqlURL = `${apiHostname}/graphql`;
 const loginURL = `${apiHostname}/login`;
 const sessionOptions = {
